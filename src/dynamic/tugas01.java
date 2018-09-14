bc4d2b6e56b13bf5f139bed33d4f8f52ddf7fb3b
wonder woman <wonder woman@LAPTOP-JRCMS98G>
2018-09-15 05:37:15 +0700

    Tambah tampil hasil

---
+++ src/dynamic/tugas01.java
@@ -0,0 +1,15 @@
+
+package dynamic;
+import java.util.Scanner;
+public class tugas01 {
+    public static void TampilJudul(String identitas)
+    {
+        System.out.println("Identitas : " + identitas);
+    }
+  public static void main(String[] args)
+    {
+        String identitas = "Sayyidati Khadijah / X RPL 2 / 32";
+        TampilJudul(identitas);
+        System.out.println("    ");
+    }
+}
