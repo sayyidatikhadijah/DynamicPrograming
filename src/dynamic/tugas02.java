4644e46f8234bdebc54c0ac62e96ed7515b3b64a
wonder woman <wonder woman@LAPTOP-JRCMS98G>
2018-09-15 07:20:12 +0700

    proses

--- src/dynamic/tugas02.java
+++ src/dynamic/tugas02.java
@@ -14,4 +14,21 @@ public class tugas02 {
         System.out.println("    ");
         
     {
-      
+         Scanner sistem = new Scanner(System.in);
+        int arr[]= {7, 5, 4, 3, 2};
+        int arr1[]= {10000, 7000, 5000, 3000, 2000};
+        int bayaran = 0;
+        
+        System.out.print("Jumlah berat maksimal : ");
+        int n = sistem.nextInt();
+        
+        for (int i = 0; i < arr.length; i++) {
+            int hasil =  n / arr[i];
+            n = n - (arr[i] * hasil);
+            bayaran += hasil * arr1[i];
+        }
+        System.out.println("Bayaran yang didapat : " + bayaran);
+    }
+    }
+}
+
