477a5edfcbe590d808c01c04018e21456a607acc
wonder woman <wonder woman@LAPTOP-JRCMS98G>
2018-09-15 07:19:47 +0700

    identitastugas2

---
+++ src/dynamic/tugas02.java
@@ -0,0 +1,17 @@
+
+package dynamic;
+import java.util.Scanner;
+
+public class tugas02 {
+     public static void TampilJudul(String identitas)
+    {
+        System.out.println("Identitas : " + identitas);
+    }
+  public static void main(String[] args)
+    {
+        String identitas = "Sayyidati Khadijah / X RPL 2 / 32";
+        TampilJudul(identitas);
+        System.out.println("    ");
+        
+    {
+      
