ce934a92a988e5fcc22e7e11cff72ea0c993da12
wonder woman <wonder woman@LAPTOP-JRCMS98G>
2018-09-15 07:16:53 +0700

    proses

--- src/dynamic/HitungFibonancci.java
+++ src/dynamic/HitungFibonancci.java
@@ -29,5 +29,25 @@ public class HitungFibonancci {
         int n = scanner.nextInt();
         return n;
     }
-}  
     
+    public static BigInteger fibo(int n)
+    {
+        BigInteger[] hasil = new BigInteger[n];
+        
+        hasil[0] = BigInteger.ONE;
+        hasil[1] = BigInteger.ONE;
+        
+        for(int i = 2; i < n; i++)
+        {
+            hasil[i] = hasil[i-1].add(hasil[i-2]);
+        }
+        
+        return hasil[n-1];
+    }
+    
+    public static void tampilHasil(int n, BigInteger hasil)
+    {
+        System.out.println("Bilangan Fibonacci ke-" + n + " : " + hasil);
+    }
+}
+
